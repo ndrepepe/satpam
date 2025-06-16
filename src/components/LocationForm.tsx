60a2e312910691a30744bf2be61b228172ec1cec
[dyad] <git@dyad.sh>
2025-06-16 19:28:08 +0700

    [dyad] Memperbaiki impor QRCode dengan namespace import. - wrote 1 file(s)

--- src/components/LocationForm.tsx
+++ src/components/LocationForm.tsx
@@ -7,7 +7,7 @@ import { Input } from '@/components/ui/input';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { toast } from 'sonner';
 import { supabase } from '@/integrations/supabase/client';
-import QRCode from 'qrcode.react'; // Mengembalikan ke impor default
+import * as QRCode from 'qrcode.react'; // Mengubah impor menjadi namespace import
 import { v4 as uuidv4 } from 'uuid';
 
 const locationSchema = z.object({
@@ -93,7 +93,7 @@ const LocationForm = () => {
             <CardTitle>QR Code untuk {locationName}</CardTitle>
           </CardHeader>
           <CardContent className="flex flex-col items-center justify-center">
-            <QRCode
+            <QRCode.QRCode // Menggunakan QRCode.QRCode
               id="qrcode-canvas"
               value={qrCodeValue}
               size={256}
