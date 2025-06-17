ac346f173fb53e8ec4f836b6167301bf49245b20
[dyad] <git@dyad.sh>
2025-06-17 22:39:20 +0700

    [dyad] Memperbaiki error 'Input is not defined' di SatpamSchedule.tsx. - wrote 1 file(s)

--- src/components/SatpamSchedule.tsx
+++ src/components/SatpamSchedule.tsx
@@ -27,6 +27,7 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog';
 import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input'; // Import Input component
 import * as XLSX from 'xlsx'; // Import xlsx
 
 interface SatpamProfile {
