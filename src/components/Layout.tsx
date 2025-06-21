34acaa8610acbbbe0d1a07eb96bc336ede3e4a3d
[dyad] <git@dyad.sh>
2025-06-21 20:00:38 +0700

    [dyad] Membuat komponen Layout yang hilang. - wrote 1 file(s)

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './Navbar';
+import { MadeWithDyad } from './made-with-dyad';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <MadeWithDyad />
+    </div>
+  );
+};
+
+export default Layout;
