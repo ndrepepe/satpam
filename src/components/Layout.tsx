643d1d1540d9a417241256659ce8a8da71b4aeb3
[dyad] <apriyo@gmail.com>
2026-05-13 09:19:33 +0700

    [dyad] Memperbaiki masalah scrollbar yang hilang dengan menyederhanakan manajemen overflow pada body - wrote 2 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
       <Navbar />
-      <main className="flex-1 w-full overflow-y-auto">
+      <main className="flex-1 w-full">
         {children}
       </main>
     </div>
